0687cb000ff592cf4e29cabba232621f2b9268c4
nbxx <DevLib@outlook.com>
2017-01-21 02:35:50 -0500

    1 [NEW] add azure storage shared project

---
+++ Source/DevLib.Azure/DevLib.Azure.Storage/BlobStorage.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BlobStorage.cs" company="Yu Guan Corporation">
+//     Copyright (c) Yu Guan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.Azure.Storage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class BlobStorage
+    {
+    }
+}
