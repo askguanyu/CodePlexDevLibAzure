fb42d14941ffcfa839ce05645bb4cc4b62ef384f
nbxx <DevLib@outlook.com>
2017-01-30 01:15:36 -0500

    1 [NEW] add queue storage unit test;

---
+++ Source/DevLib.Azure.NET45.UnitTest/Given_QueueStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevLib.Azure.Storage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevLib.Azure.NET45.UnitTest
+{
+    [TestClass]
+    public class Given_QueueStorage
+    {
+        [TestMethod]
+        public void When_Enqueue()
+        {
+            var queueClient = QueueClient.DevelopmentClient;
+            var queue = queueClient.GetQueueStorage("queue1");
+
+            queue.Enqueue("msg1", TimeSpan.FromSeconds(20), TimeSpan.FromSeconds(10));
+        }
+    }
+}
