e5fc660e88d3ba2b41bbf40d9b1631e287e9c6b6
nbxx <DevLib@outlook.com>
2017-01-31 18:46:47 -0500

    1 [NEW] add logger file;

---
+++ Source/DevLib.Azure.Logging/Logger.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Logger.cs" company="YuGuan Corporation">
+//     Copyright (c) YuGuan Corporation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DevLib.Azure.Logging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.WindowsAzure.Storage.Table;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Logger
+    {
+    }
+}
--- Source/DevLib.Azure.Logging/LoggingLevel.cs
+++ Source/DevLib.Azure.Logging/LoggingLevel.cs
@@ -11,33 +11,38 @@ namespace DevLib.Azure.Logging
     public enum LoggingLevel
     {
         /// <summary>
+        /// Represents turn off message.
+        /// </summary>
+        OFF = 0,
+
+        /// <summary>
         /// Represents debug message level. Very detailed logs, which may include high-volume information such as protocol payloads. This log level is typically only enabled during development.
         /// </summary>
-        DBUG = 0,
+        DBG = 1,
 
         /// <summary>
         /// Represents information message level. Which are normally enabled in production environment.
         /// </summary>
-        INFO = 1,
+        INF = 2,
 
         /// <summary>
         /// Represents warning message level. Typically for non-critical issues, which can be recovered or which are temporary failures.
         /// </summary>
-        WARN = 2,
+        WRN = 3,
 
         /// <summary>
         /// Represents exception message level.
         /// </summary>
-        EXCP = 3,
+        EXP = 4,
 
         /// <summary>
         /// Represents error message level.
         /// </summary>
-        ERRO = 4,
+        ERR = 5,
 
         /// <summary>
         /// Represents fatal or critical message level.
         /// </summary>
-        FAIL = 5,
+        FAL = 6,
     }
 }
